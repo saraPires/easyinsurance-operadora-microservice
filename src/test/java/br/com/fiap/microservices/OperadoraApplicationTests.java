3af490bcf3cf1d9399138db08cadb7adee05cd0d
saraPiresDev <sararegina.pires@gmail.com>
2021-03-07 21:38:15 -0300

    Alterando a classe de testes com a operadora.

--- src/test/java/br/com/fiap/microservices/OperadoraApplicationTests.java
+++ src/test/java/br/com/fiap/microservices/OperadoraApplicationTests.java
@@ -8,18 +8,26 @@ import java.net.URISyntaxException;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus;
+
+import br.com.fiap.microservices.easyinsurance.operadora.OperadoraApplication;
 import br.com.fiap.microservices.easyinsurance.operadora.entity.Operadora;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = OperadoraApplication.class)
+@AutoConfigureWebMvc
 public class OperadoraApplicationTests {
 
  	@Autowired
@@ -50,11 +58,36 @@ public class OperadoraApplicationTests {
          
         assertEquals(201, result.getStatusCodeValue());
     }
+    
+    
+    @Test
+    public void testeCriandoPlanoValoresNulos() throws URISyntaxException 
+    {
+    	final String baseUrl = "http://localhost:"+randomServerPort+"/easyinsurance/operadora";
+        URI uri = new URI(baseUrl);
+    	Operadora operadora = new Operadora();
+    	
+    	operadora.setRazaoSocialOperdora("CAIXA DE ASSISTÊNCIA DO SETOR DE ENERGIA -EVIDA");
+    	operadora.setCnpj(null);
+    	operadora.setCodigoRegistroAns(418374l);
+    	operadora.setNomeFantasia("E-VIDA");
+    	operadora.setModalidade("Autogestão");
+         
+        HttpHeaders headers = new HttpHeaders();
+ 
+        HttpEntity<Operadora> request = new HttpEntity<>(operadora, headers);
+         
+        ResponseEntity<String> result = this.restTemplate.postForEntity(uri, request, String.class);
+         
+        
+        assertEquals(HttpStatus.SC_CONFLICT, result.getStatusCodeValue());
+        assertEquals(false, result.getBody().contains("Missing request header"));
+    }
 
     
-	
-	@Test
-	void contextLoads() {
-	}
+    
+    
+    
+    
+}	
 
-}

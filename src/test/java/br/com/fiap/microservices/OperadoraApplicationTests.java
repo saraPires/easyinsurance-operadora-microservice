e207809d376bf62b2c0db3378cabced68d0865ef
saraPiresDev <sararegina.pires@gmail.com>
2021-03-07 21:24:32 -0300

    Testes operadora inclusão.

--- src/test/java/br/com/fiap/microservices/OperadoraApplicationTests.java
+++ src/test/java/br/com/fiap/microservices/OperadoraApplicationTests.java
@@ -1,12 +1,22 @@
 package br.com.fiap.microservices;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.fiap.microservices.easyinsurance.operadora.entity.Operadora;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -18,7 +28,30 @@ public class OperadoraApplicationTests {
     @LocalServerPort
     int randomServerPort;
     
+    @Test
+    public void testeCriandoOperadora() throws URISyntaxException 
+    {
+    	final String baseUrl = "http://localhost:"+randomServerPort+"/easyinsurance/operadora";
+    	URI uri = new URI(baseUrl);
+    	Operadora operadora = new Operadora();
+    	
+    	operadora.setRazaoSocialOperdora("CAIXA DE ASSISTÊNCIA DO SETOR DE ENERGIA -EVIDA");
+    	operadora.setCnpj(123456789000812l);
+    	operadora.setCodigoRegistroAns(418374l);
+    	operadora.setNomeFantasia("E-VIDA");
+    	operadora.setModalidade("Autogestão");
+    	
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-COM-PERSIST", "true");      
+ 
+        HttpEntity<Operadora> request = new HttpEntity<>(operadora, headers);
+         
+        ResponseEntity<String> result = this.restTemplate.postForEntity(uri, request, String.class);
+         
+        assertEquals(201, result.getStatusCodeValue());
+    }
 
+    
 	
 	@Test
 	void contextLoads() {
